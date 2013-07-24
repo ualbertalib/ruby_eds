a1893d88462f0fd14be1a28b4f209ba9dee9f7c4
Sam Popowich <spopowich@gmail.com>
2013-07-24 15:16:34 -0600

    Trying to get CreateSession working.

--- test.rb
+++ test.rb
@@ -9,11 +9,13 @@ password = args['password']
 
 auth_json = "{\"UserId\":\"#{username}\",\"Password\":\"#{password}\",\"InterfaceId\":\"WSapi\"}"
 response = open('https://eds-api.ebscohost.com/authservice/rest/UIDAuth', :method=>:post, :body => auth_json, 'Content-Type' => 'application/json')
-puts response.read
-#doc = Nokogiri::XML(response.read)
-#doc.remove_namespaces!
-#auth_token = doc.xpath("//AuthToken").inner_text
-#puts auth_token
-#session_info = open("http://eds-api.ebscohost.com/edsapi/rest/createsession?WSapi", :method=>:get, "x-authenticationToken"=>auth_token)
-#puts session_info
+#puts response.read
+doc = Nokogiri::XML(response.read)
+doc.remove_namespaces!
+auth_token = doc.xpath("//AuthToken").inner_text
+puts auth_token
+
+session_json = '{"Profile":"api_demo","Guest":"n"}'
+session_info = open("http://eds-api.ebscohost.com/edsapi/rest/CreateSession", :method=>:get, :body=>session_json, "x-authenticationToken"=>auth_token)
+puts session_info.read
 
